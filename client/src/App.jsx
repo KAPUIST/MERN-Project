5ce19a064ba239b641599d77d707da0367e8a1ba
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-09-05 19:08:33 +0900

    feat/react initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
