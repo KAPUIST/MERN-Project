ee212390f3bd518e47754a85b3e94dcf5d2b194a
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-09-05 19:53:16 +0900

    feat/react header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import SignIn from './pages/SignIn';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
